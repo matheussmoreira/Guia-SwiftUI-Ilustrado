6afcf0d2b265cd3e82cb4d9048dcbdf2af2bebbf
Ricardo Venieris <rvenieris@gmail.com>
2021-01-26 15:05:10 -0300

    projeto zerado

---
+++ GuiaSwiftUI/GuiaSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuiaSwiftUI
+//
+//  Created by Ricardo Venieris on 26/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GuiaSwiftUI/GuiaSwiftUI/GuiaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuiaSwiftUIApp.swift
+//  GuiaSwiftUI
+//
+//  Created by Ricardo Venieris on 26/01/21.
+//
+
+import SwiftUI
+
+@main
+struct GuiaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
