62bc4ed86b5eb2d82b697e4aa04473a9dfdd6c6c
matheussmoreira <matheussmoreira100@gmail.com>
2021-09-24 22:03:20 -0300

    TextField: isEditing, onCommit e .autocapitalization

--- GuiaSwiftUI/GuiaSwiftUI/ComponentesUI/ModulosComp/MyTextField.swift
+++ GuiaSwiftUI/GuiaSwiftUI/ComponentesUI/ModulosComp/MyTextField.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MyTextField_Previews: PreviewProvider {
     static var previews: some View {
-        MyTextField.Exemplo1()
+        MyTextField.Autocapitalizacao()
     }
 }
 
@@ -29,6 +29,52 @@ struct MyTextField {
                     TextField("Digite aqui", text: $texto)
                 }.padding(.horizontal)
             }
+        }
+    }
+    
+    struct EdicaoConfirmacao: View {
+        @State private var texto = ""
+        @State private var digitando = false
+        
+        var body: some View {
+            VStack {
+                HStack {
+                    Text(digitando ? "Você está digitando!" : "Esperando digitar...")
+                        .font(.headline)
+                    Spacer()
+                }
+                TextField("Digite aqui", text: $texto) { isEditing in
+                    digitando = isEditing
+                } onCommit: {
+                    print("Você pressionou return!")
+                }
+            }.padding(.horizontal)
+        }
+    }
+    
+    struct Autocapitalizacao: View {
+        @State private var texto1 = ""
+        @State private var texto2 = ""
+        @State private var texto3 = ""
+        @State private var texto4 = ""
+        
+        var body: some View {
+            VStack {
+                HStack {
+                    Text("Digite abaixo:")
+                        .font(.headline)
+                    Spacer()
+                }
+                TextField(".allCharacters", text: $texto1)
+                    .autocapitalization(.allCharacters)
+                TextField(".sentences", text: $texto2)
+                    .autocapitalization(.sentences)
+                TextField(".words", text: $texto3)
+                    .autocapitalization(.words)
+                TextField(".none", text: $texto4)
+                    .autocapitalization(.none)
+                
+            }.padding(.horizontal)
         }
     }
 }
