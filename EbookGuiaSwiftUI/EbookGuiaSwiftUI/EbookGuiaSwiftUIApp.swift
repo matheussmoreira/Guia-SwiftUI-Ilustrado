4f5ac12fa25ee5470df3580796f3bc7c41f160d2
Matheus S. Moreira <matheussmoreira100@gmail.com>
2023-02-06 21:47:32 -0300

    Novo projeto xcode

---
+++ EbookGuiaSwiftUI/EbookGuiaSwiftUI/EbookGuiaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EbookGuiaSwiftUIApp.swift
+//  EbookGuiaSwiftUI
+//
+//  Created by Matheus S. Moreira on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct EbookGuiaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
