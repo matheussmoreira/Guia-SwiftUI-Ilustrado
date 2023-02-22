e9e6f212a32bb7972f2d8f34bf92c0d10b667c39
Matheus S. Moreira <matheussmoreira100@gmail.com>
2023-02-22 15:24:23 -0300

    Novo projeto

---
+++ GuiaSwiftUIEbook/GuiaSwiftUIEbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuiaSwiftUIEbook
+//
+//  Created by Matheus S. Moreira on 22/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Aprenda a programar!")
+            .font(.title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
